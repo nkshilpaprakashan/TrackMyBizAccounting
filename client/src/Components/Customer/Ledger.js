05516db2b9c5a7496a2892bcb85a61e21c013fd1
N K SHILPA PRAKASHAN <nk-shilpa.prakashan@ibm.com>
2026-05-03 23:17:19 +0530

    Added ledger

--- client/src/Components/Customer/Ledger.js
+++ client/src/Components/Customer/Ledger.js
@@ -46,7 +46,7 @@ const columns = [
 ];
 
 const [getuserproductdata, setProductdata] = useState([]);
-console.log(getuserproductdata);
+console.log("getuserproductdata",getuserproductdata);
 
 const data = async () => {
 
